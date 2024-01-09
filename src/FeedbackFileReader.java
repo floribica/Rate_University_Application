c4a5751a4f9f6349eb96d06284fa434f12ff444f
fabianlika <fabjan.lika@fti.edu.al>
2024-01-09 13:24:20 +0100

    file class

--- src/FeedbackFileReader.java
+++ src/FeedbackFileReader.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,16 +13,19 @@ public class FeedbackFileReader {
         try {
             File file = new File("C:\\Users\\lenovo\\Desktop\\Rate_University_Application\\feedbacks.txt");
             Scanner input = new Scanner(file);
-            while (input.hasNext()) {
+            while (input.hasNextLine()) {
                 String line = input.nextLine();
-                String[] feedbackArray = line.split(" ");
-                // Assuming appropriate constructor exists in the Feedback class
-                Feedback feedback = new Feedback(feedbackArray[0], Integer.parseInt(feedbackArray[1]),
-                        feedbackArray[2], feedbackArray[3]);
-                feedbacks.add(feedback);
+                String[] feedbackArray = line.split(" ", 4); // Assuming the first three elements represent the feedback attributes
+                if (feedbackArray.length >= 4) {
+                    // Assuming appropriate constructor exists in the Feedback class
+                    Feedback feedback = new Feedback(feedbackArray[0], Integer.parseInt(feedbackArray[1]), feedbackArray[2], feedbackArray[3]);
+                    feedbacks.add(feedback);
+                } else {
+                    System.err.println("Error: Incomplete feedback data for line: " + line);
+                }
             }
             input.close();
-        } catch (IOException e) {
+        } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
         }
         return feedbacks;
